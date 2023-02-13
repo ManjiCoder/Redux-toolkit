800ee0e50da2f7cda2f7461e733fd7ff0984639e
ManjiCoder <manjicoder@gmail.com>
2023-02-13 23:17:19 +0530

    Video-3 ✔

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,12 +16,12 @@ function Navbar() {
   return (
     <header className="p-4 text-center text-gray-400">
       <h6>Weclome to redux-toolkit</h6>
-      <h1 ref={zoomIn} className="text-6xl font-bold uppercase my-8 transition-transform duration-500 ease-in-out">
+      <h1 ref={zoomIn} className="text-4xl lg:text-6xl font-bold uppercase my-8 transition-transform duration-500 ease-in-out">
         Admin table
       </h1>
       <h6>One Destination for complete Web Development</h6>
       <nav>
-        <ul className="mt-5 flex text-xl font-semibold space-x-3 justify-between bg-slate-100 lg:w-4/6 mx-auto px-5">
+        <ul className="mt-5 flex lg:text-xl font-semibold space-x-3 justify-between bg-slate-100 lg:w-4/6 mx-auto px-5 rounded-md shadow-md">
           {li.map((item) => (
             <li key={item} className="py-3 uppercase cursor-pointer">
               {item}
