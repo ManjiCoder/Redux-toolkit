c36bf1386c0927a88a25d067bb3362854c6d8df2
ManjiCoder <manjicoder@gmail.com>
2023-03-09 01:13:37 +0530

    video - 08 ✔ Redux Toolkit #8: Deleting a Single User from State 🔥

--- src/components/DisplayUsers.jsx
+++ src/components/DisplayUsers.jsx
@@ -1,23 +1,36 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import { useSelector } from 'react-redux';
-import tw from 'tailwind-styled-components';
+import { motion } from 'framer-motion';
+import { MdDeleteForever } from 'react-icons/md';
 import HrLine from './HrLine';
+import { removeUser } from '../store/slices/UserSlice';
 
 function DisplayUsers() {
   const data = useSelector((state) => state.users);
   console.log(data);
   return (
     <>
-      {data.map((user) => (
+      {data.map((user, id) => (
         <>
-          <UserInfo key={user}>{user}</UserInfo>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.25 }}
+            key={user}
+            className="flex font-semibold text-gray-500 justify-between items-center text-xl"
+          >
+            {user}
+            <button type="button" className="text-3xl text-red-600 cursor-pointer hover:scale-110 transition-[scale]" onClick={() => removeUser(id)}>
+              <MdDeleteForever />
+            </button>
+
+          </motion.div>
           <HrLine />
         </>
       ))}
     </>
   );
 }
-const UserInfo = tw.div`
 
-`;
 export default DisplayUsers;
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -34,10 +34,10 @@ lg:w-4/6 mx-auto px-11 py-4 flex flex-col
 const UserListWrapper = tw.div`
 flex justify-between items-center`;
 
-const UserList = tw.div`text-xl lg:text-2xl text-gray-500`;
+const UserList = tw.div`text-xl lg:text-2xl text-gray-500 font-semibold`;
 const AddNewUserButton = tw.button`
-w-36 bg-gradient-to-r from-indigo-700 to-blue-500 text-white py-2 rounded-md shadow-sm shadow-slate-500 focus:ring-4 transition-[focus:ring-4] duration-200 ring-blue-300 place-self-end
+w-36 bg-gradient-to-r from-indigo-700 to-blue-500 text-white py-2 rounded-md shadow-sm shadow-slate-500 focus:ring-4 transition-[focus:ring-4] duration-200 ring-blue-300 place-self-end hover:scale-105
 `;
 const ClearUserButton = tw.button`
-w-36 bg-gradient-to-r from-red-600 to-red-500 text-white py-2 rounded-md shadow-sm shadow-slate-500 focus:ring-4 transition-[focus:ring-4] duration-200 ring-red-300 place-self-end
+w-36 bg-gradient-to-r from-red-600 to-red-500 text-white py-2 rounded-md shadow-sm shadow-slate-500 focus:ring-4 transition-[focus:ring-4] duration-200 ring-red-300 place-self-end hover:scale-105
 `;
